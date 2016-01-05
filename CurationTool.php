5fdb6f57fb98db080d02af017d8e6a4ec2450bd0
prasanna.bankuru <bankuruprasannakumar@users.noreply.github.com>
2016-01-05 14:14:30 +0530

    Update CurationTool.php

--- CurationTool.php
+++ CurationTool.php
@@ -63,6 +63,11 @@
                 $category = $_POST['category'];
                 echo "category = '$category';";
             }
+            $merchant = "all";
+           if (isset($_POST['merchant'])) {
+                $merchant = $_POST['merchant'];
+                echo "merchant = '$merchant';";
+            }
             echo "</script>";
         ?>
         
@@ -88,7 +93,8 @@
                             alert("Products Loaded");
                         }
                     }
-                    productRequest1.send(search_query);
+                     var JSONObj = { "keyword":search_query, "category":category, "merchant":merchant};
+                     productRequest1.send(JSON.stringify(JSONObj)); 
                     
                     var articleRequest = new XMLHttpRequest;
                     articleRequest.open("get", "GetArticles.php?q=" + search_query + "&category=" + category, true);
@@ -228,16 +234,18 @@
                     </select>
                 </div>
 
-                <div class = "param">MERCHANT: &nbsp;
-                    <select>
-                        <option value = "All">All</option>
-                        <option value = "Flipkart">Flipkart</option>
-                        <option value = "Amazon">Amazon</option>
-                        <option value = "Jabong">Jabong</option>
-                        <option value = "BestBuy">BestBuy</option>
-                    </select>
-                </div>
 
+                <div class = "param">Merchant: &nbsp;
+                    <select name = "merchant" onchange = "setMerchant(this.value)">
+                        <option value = "bingProducts" <?php if ($merchant == "bingProducts") { echo "selected"; } ?>>Bing Products</option>
+                        <option value = "bingArticles" <?php if ($merchant == "bingArticles") { echo "selected"; } ?>>Bing Articles</option>
+                        <option value = "all" <?php if ($merchant == "all") { echo "selected"; } ?>>ALL</option>
+<!--                         <option value = "sports" <?php if ($merchant == "sports") { echo "selected"; } ?>>Sports and Fitness</option>
+                        <option value = "music" <?php if ($merchant == "music") { echo "selected"; } ?>>Music</option>
+                        <option value = "home" <?php if ($merchant == "home") { echo "selected"; } ?>>Home Decor</option>
+                        <option value = "life-hacks" <?php if ($merchant == "life-hacks") { echo "selected"; } ?>>Life Hacks</option>
+ -->                    </select>
+                 </div> 
                 <div class = "param">TYPE: &nbsp;
                     <select name = "categoryO" onchange = "setCategory(this.value)">
                         <option value = "fashion" <?php if ($category == "fashion") { echo "selected"; } ?>>Fashion</option>
